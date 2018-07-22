8b581d82d0426c2b6ffc9b98c5d566de44795e4b
Oshiro <aoshiro@slc.moog.com>
2018-07-21 23:04:06 -0600

    Final upload

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,11 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-var friends = [
+
+var friendsData = [
     {
-        "name":"Brian O' Conner",
-        "photo":"https://media.giphy.com/media/10o2NUd9uVRzLG/giphy.gif",
-        "scores":[
+        name:"Brian O' Conner",
+        photo:"https://media.giphy.com/media/10o2NUd9uVRzLG/giphy.gif",
+        scores:[
            5,
-           1,
+           3,
            4,
            4,
            5,
@@ -18,154 +13,154 @@ var friends = [
            2,
            5,
            4,
-           1
+           4
             ]
         },
     {
-    "name":"Dominique",
-    "photo":"https://media3.giphy.com/media/ZpWmnRYVrwn6w/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       4,
-       5,
+    name:"Dominique",
+    photo:"https://media3.giphy.com/media/ZpWmnRYVrwn6w/giphy.gif",
+    scores:[
+       2,
        1,
        2,
-       5,
+       4,
+       2,
+       1,
+       2,
+       1,
+       3,
+       1
+        ]
+  },
+  {
+    name:"Seinfeld",
+    photo:"https://media.giphy.com/media/cGsWzE70hte7K/giphy.gif",
+    scores:[
+       1,
+       1,
+       3,
+       4,
+       3,
+       1,
+       2,
+       3,
        4,
        1
         ]
   },
   {
-    "name":"Seinfeld",
-    "photo":"https://media.giphy.com/media/cGsWzE70hte7K/giphy.gif",
-    "scores":[
-       9,
-       1,
-       4,
-       4,
+    name:"Jay Leno",
+    photo:"https://media.giphy.com/media/l2TuR9r82FR0k/giphy.gif",
+    scores:[
        5,
        1,
+       1,
+       3,
        2,
-       5,
-       4,
-       1
+       3,
+       2,
+       1,
+       3,
+       3
         ]
   },
   {
-    "name":"Jay Leno",
-    "photo":"https://media.giphy.com/media/l2TuR9r82FR0k/giphy.gif",
-    "scores":[
+    name:"Jeremy Clarkson",
+    photo:"https://media.giphy.com/media/3ohc0V0VeB6RekdA8E/giphy.gif",
+    scores:[
        5,
        1,
        4,
        3,
        2,
-       6,
-       9,
-       8,
-       9,
-       9
+       2,
+       3,
+       3,
+       2,
+       2
         ]
   },
   {
-    "name":"Jeremy Clarkson",
-    "photo":"https://media.giphy.com/media/3ohc0V0VeB6RekdA8E/giphy.gif",
-    "scores":[
+    name:"Ayrton Senna",
+    photo:"https://media.giphy.com/media/8mgQWF7hk5m3XDj3pM/giphy.gif",
+    scores:[
        5,
        1,
        4,
        3,
        2,
-       6,
-       9,
-       8,
-       9,
-       9
+       3,
+       3,
+       3,
+       3,
+       3
         ]
   },
   {
-    "name":"Ayrton Senna",
-    "photo":"https://media.giphy.com/media/8mgQWF7hk5m3XDj3pM/giphy.gif",
-    "scores":[
+    name:"Fernando Alonso",
+    photo:"https://media.giphy.com/media/vX7cqnswikone/giphy.gif",
+    scores:[
        5,
        1,
        4,
        3,
        2,
-       6,
-       9,
-       8,
-       9,
-       9
+       2,
+       3,
+       3,
+       1,
+       2
         ]
   },
   {
-    "name":"Fernando Alonso",
-    "photo":"https://media.giphy.com/media/vX7cqnswikone/giphy.gif",
-    "scores":[
+    name:"Marty Mcfly",
+    photo:"https://media.giphy.com/media/UXxgCMnkyeFLa/giphy.gif",
+    scores:[
        5,
        1,
        4,
        3,
        2,
-       6,
-       9,
-       8,
-       9,
-       9
+       2,
+       2,
+       3,
+       2,
+       2
         ]
   },
   {
-    "name":"Marty Mcfly",
-    "photo":"https://media.giphy.com/media/UXxgCMnkyeFLa/giphy.gif",
-    "scores":[
+    name:"Lewis Hamilton",
+    photo:"https://media.giphy.com/media/5b5OWakvTmIS3yB7cr/giphy.gif",
+    scores:[
+       5,
+       2,
+       4,
+       3,
+       2,
+       2,
+       2,
+       3,
+       4,
+       4
+        ]
+  },
+  {
+    name:"Doc Brown",
+    photo:"https://media.giphy.com/media/LefVVnt3JAvHa/giphy.gif",
+    scores:[
        5,
        1,
        4,
        3,
        2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
-  },
-  {
-    "name":"Lewis Hamilton",
-    "photo":"https://media.giphy.com/media/5b5OWakvTmIS3yB7cr/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
-       3,
        2,
-       6,
-       9,
-       8,
-       9,
-       9
-        ]
-  },
-  {
-    "name":"Doc Brown",
-    "photo":"https://media.giphy.com/media/LefVVnt3JAvHa/giphy.gif",
-    "scores":[
-       5,
-       1,
-       4,
        3,
-       2,
-       6,
-       9,
-       8,
-       9,
-       9
+       4,
+       4,
+       3
         ]
   }
   ]
   
   // Note how we export the array. This makes it accessible to other files using require.
-  module.exports = friends;
+  module.exports = friendsData;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,58 +1,77 @@
-var friendData 		= require('../data/friends.js');
-var path 			= require('path');
-
-// API GET Requests - when users "visit" a page. 
-// (ex:localhost:PORT/api/admin...they are shown a JSON of the data in the table) 
-
-var totalDifference = 0;
-
-module.exports = function(app){
-	app.get('/api/friends', function(req, res){
-		res.json(friends);
-	});
-
-//API POST Request-handles when user submits a form & thus submits data to the server.
-// In each of the below cases, when a user submits form data (a JSON object)
-// ...the JSON is pushed to the appropriate Javascript array
+console.log('API Route Connected Successfully');
 
 
-	app.post('/api/friends', function(req, res){
+// Link in Friends Data
+var friendsData = require('../data/friends.js');
 
-		var greatMatch = {
-			name: "",
-			image: "",
-			matchDifference: 1000
-		};
-		var usrData 	= req.body;
-		var usrName 	= usrData.name;
-		var usrImage 	= usrData.image;
-		var usrScores 	= usrData.scores;
 
-		var totalDifference = 0;
+// Includes Two Routes
+function apiRoutes(app) {
 
-		//loop through the friends data array of objects to get each friends scores
-		for(var i = 0; i < [friends].length-1; i++){
-			console.log(friends[i].name);
-			totalDifference = 0;
+  // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
+  app.get('/api/friends', function (req, res) {
+    res.json(friendsData);
+  });
 
-			//loop through that friends score and the users score and calculate the 
-			// absolute difference between the two and push that to the total difference variable set above
-			for(var j = 0; j < 10; j++){
-				// We calculate the difference between the scores and sum them into the totalDifference
-				totalDifference += Math.abs(parseInt(usrScores[j]) - parseInt(friends[i].scores[j]));
-				// If the sum of differences is less then the differences of the current "best match"
-				if (totalDifference <= greatMatch.friendDifference){
+  // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
+  app.post('/api/friends', function (req, res) {
 
-					// Reset the bestMatch to be the new friend. 
-					greatMatch.name = friends[i].name;
-					greatMatch.photo = friends[i].photo;
-					greatMatch.matchDifference = totalDifference;
-				}
-			}
-		}
+    // Parse new friend input to get integers (AJAX post seemed to make the numbers strings)
+    var newFriend = {
+      name: req.body.name,
+      photo: req.body.photo,
+      scores: []
+    };
+    var scoresArray = [];
+    for(var i=0; i < req.body.scores.length; i++){
+      scoresArray.push( parseInt(req.body.scores[i]) )
+    }
+    newFriend.scores = scoresArray;
 
-		friends.push(usrData);
- 
-		res.json(greatMatch);
-	});
-};
+
+    // Cross check the new friend entry with the existing ones
+    var scoreComparisionArray = [];
+    for(var i=0; i < friendsData.length; i++){
+
+      // Check each friend's scores and sum difference in points
+      var currentComparison = 0;
+      for(var j=0; j < newFriend.scores.length; j++){
+        currentComparison += Math.abs( newFriend.scores[j] - friendsData[i].scores[j] );
+      }
+
+      // Push each comparison between friends to array
+      scoreComparisionArray.push(currentComparison);
+    }
+
+    // Determine the best match using the postion of best match in the friendsData array
+    var bestMatchPosition = 0; // assume its the first person to start
+    for(var i=1; i < scoreComparisionArray.length; i++){
+      
+      // Lower number in comparison difference means better match
+      if(scoreComparisionArray[i] <= scoreComparisionArray[bestMatchPosition]){
+        bestMatchPosition = i;
+      }
+
+    }
+
+    // ***NOTE*** If the 2 friends have the same comparison, then the NEWEST entry in the friendsData array is chosen
+    var bestFriendMatch = friendsData[bestMatchPosition];
+
+
+
+    // Reply with a JSON object of the best match
+    res.json(bestFriendMatch);
+
+
+
+    // Push the new friend to the friends data array for storage
+    friendsData.push(newFriend);
+
+  });
+
+}
+
+
+// Export for use in main server.js file
+module.exports = apiRoutes;
+
